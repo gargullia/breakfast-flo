gargullia/breakfast-flo

commit c5d5bf938620a8597577f1efb0087273c584fd0b

ASoC: wcd9310: Use sound control data to expose sound gains
Register a new sound control data to take advantage of the sysfs-based
sound gain controller and export codec's audio lines' values to SysFS.

Change-Id: I796a08b4cd8ead89c471fcf327ec21a4c343b1df
Signed-off-by: Alex Saiko <solcmdr@gmail.com>

sound/soc/codecs/wcd9310.c
@@ -22,6 +22,7 @@
 #include <linux/mfd/wcd9xxx/core.h>
 #include <linux/mfd/wcd9xxx/wcd9xxx_registers.h>
 #include <linux/mfd/wcd9xxx/wcd9310_registers.h>
+#include <linux/mfd/wcd9xxx/wcd9xxx-snd-ctrl.h>
 #include <linux/mfd/wcd9xxx/pdata.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
@@ -272,6 +273,7 @@ static struct hpf_work tx_hpf_work[NUM_DECIMATORS];
 
 struct tabla_priv {
 	struct snd_soc_codec *codec;
+	struct snd_ctrl_data *ctrl_data;
 	struct tabla_reg_address reg_addr;
 	u32 adc_count;
 	u32 cfilt1_cnt;
@@ -8477,6 +8479,24 @@ static int tabla_codec_probe(struct snd_soc_codec *codec)
 		goto err_pdata;
 	}
 
+	tabla->ctrl_data = kzalloc(sizeof(*tabla->ctrl_data), GFP_KERNEL);
+	if (IS_ERR_OR_NULL(tabla->ctrl_data)) {
+		dev_err(codec->dev, "Failed to allocate control data\n");
+		goto fail_ctrl;
+	}
+
+	tabla->ctrl_data->codec = codec;
+	tabla->ctrl_data->name = codec->name;
+	tabla->ctrl_data->read = &tabla_read;
+	tabla->ctrl_data->write = &tabla_write;
+
+	ret = snd_ctrl_register(tabla->ctrl_data);
+	if (IS_ERR_VALUE(ret)) {
+		pr_err("%s: Unable to register control data\n", __func__);
+		kfree(tabla->ctrl_data);
+	}
+
+fail_ctrl:
 //	snd_soc_add_codec_controls(codec, tabla_snd_controls,
 //			     ARRAY_SIZE(tabla_snd_controls));
 	if (TABLA_IS_1_X(control->version))
@@ -8660,6 +8680,7 @@ err_remove_irq:
 err_insert_irq:
 err_pdata:
 	mutex_destroy(&tabla->codec_resource_lock);
+	kfree(tabla->ctrl_data);
 	kfree(tabla);
 	return ret;
 }
@@ -8688,6 +8709,9 @@ static int tabla_codec_remove(struct snd_soc_codec *codec)
 	debugfs_remove(tabla->debugfs_poke);
 	debugfs_remove(tabla->debugfs_mbhc);
 #endif
+	snd_ctrl_unregister(tabla->ctrl_data);
+
+	kfree(tabla->ctrl_data);
 	kfree(tabla);
 	return 0;
 }

commit 8a5ff7d05228bbdc373d30b6fa2d3abd8b4a0703

ASoC: wcd9310: Workaround a conflict with Android's AudioHAL
This was explained in commit 655c0eb56c0e.

Unfortunately, there is no other option, because Android's AudioHAL
actively uses IOCTL (just like any other normal client) to setup sound
lines.  If we want to completely switch controls from IOCTL to simple
sysfs, we need to entirely filter out writing to target lines from it.

Fixes: c5d5bf938620 ("ASoC: wcd9310: Use sound control data to expose sound gains")
Change-Id: Icc72320443a8ca3bb6106ded31ba7fc9d9e15ac3
Signed-off-by: Alex Saiko <solcmdr@gmail.com>

sound/soc/codecs/wcd9310.c
@@ -3975,6 +3975,31 @@ static unsigned int tabla_read(struct snd_soc_codec *codec,
 	return val;
 }
 
+static int tabla_filter(struct snd_soc_codec *codec, unsigned int reg)
+{
+	struct tabla_priv *tabla = snd_soc_codec_get_drvdata(codec);
+	struct snd_ctrl_data *snd_data = tabla->ctrl_data;
+
+	if (IS_ERR_OR_NULL(snd_data) || !snd_ctrl_data_handled(snd_data))
+		return 0;
+
+	/* Sort in use-frequency order */
+	if (reg == snd_data->speaker_l_line)
+		return -EINVAL;
+	else if (reg == snd_data->speaker_r_line)
+		return -EINVAL;
+	else if (reg == snd_data->headphone_l_line)
+		return -EINVAL;
+	else if (reg == snd_data->headphone_r_line)
+		return -EINVAL;
+	else if (reg == snd_data->cam_mic_line)
+		return -EINVAL;
+	else if (reg == snd_data->mic_line)
+		return -EINVAL;
+
+	return 0;
+}
+
 static s16 tabla_get_current_v_ins(struct tabla_priv *tabla, bool hu)
 {
 	s16 v_ins;
@@ -8720,6 +8745,7 @@ static struct snd_soc_codec_driver soc_codec_dev_tabla = {
 	.remove	= tabla_codec_remove,
 	.read = tabla_read,
 	.write = tabla_write,
+	.filter = tabla_filter,
 	.readable_register = tabla_readable,
 	.volatile_register = tabla_volatile,
 