gargullia/breakfast-flo

commit 8a5ff7d05228bbdc373d30b6fa2d3abd8b4a0703

ASoC: wcd9310: Workaround a conflict with Android's AudioHAL
This was explained in commit 655c0eb56c0e.

Unfortunately, there is no other option, because Android's AudioHAL
actively uses IOCTL (just like any other normal client) to setup sound
lines.  If we want to completely switch controls from IOCTL to simple
sysfs, we need to entirely filter out writing to target lines from it.

Fixes: c5d5bf938620 ("ASoC: wcd9310: Use sound control data to expose sound gains")
Change-Id: Icc72320443a8ca3bb6106ded31ba7fc9d9e15ac3
Signed-off-by: Alex Saiko <solcmdr@gmail.com>

sound/soc/codecs/wcd9310.c
@@ -3975,6 +3975,31 @@ static unsigned int tabla_read(struct snd_soc_codec *codec,
 	return val;
 }
 
+static int tabla_filter(struct snd_soc_codec *codec, unsigned int reg)
+{
+	struct tabla_priv *tabla = snd_soc_codec_get_drvdata(codec);
+	struct snd_ctrl_data *snd_data = tabla->ctrl_data;
+
+	if (IS_ERR_OR_NULL(snd_data) || !snd_ctrl_data_handled(snd_data))
+		return 0;
+
+	/* Sort in use-frequency order */
+	if (reg == snd_data->speaker_l_line)
+		return -EINVAL;
+	else if (reg == snd_data->speaker_r_line)
+		return -EINVAL;
+	else if (reg == snd_data->headphone_l_line)
+		return -EINVAL;
+	else if (reg == snd_data->headphone_r_line)
+		return -EINVAL;
+	else if (reg == snd_data->cam_mic_line)
+		return -EINVAL;
+	else if (reg == snd_data->mic_line)
+		return -EINVAL;
+
+	return 0;
+}
+
 static s16 tabla_get_current_v_ins(struct tabla_priv *tabla, bool hu)
 {
 	s16 v_ins;
@@ -8720,6 +8745,7 @@ static struct snd_soc_codec_driver soc_codec_dev_tabla = {
 	.remove	= tabla_codec_remove,
 	.read = tabla_read,
 	.write = tabla_write,
+	.filter = tabla_filter,
 	.readable_register = tabla_readable,
 	.volatile_register = tabla_volatile,
 

commit 903b7489ca7e7c8dcbfc568ece238f751d3d6aa4

ASoC: wcd9310: Utilize IOCTL_BYPASS flag
Allow tabla_filter to selectively pass over IOCTL write requests even if
there are sound registers that are handled by SysFS Sound Gain Controller.

Fixes: 8a5ff7d05228 ("ASoC: wcd9310: Workaround a conflict with Android's AudioHAL")
Change-Id: I0c4fd0f05e5e17180adbe8117b5c7b7b49bbd69a
Signed-off-by: Alex Saiko <solcmdr@gmail.com>

sound/soc/codecs/wcd9310.c
@@ -3980,7 +3980,8 @@ static int tabla_filter(struct snd_soc_codec *codec, unsigned int reg)
 	struct tabla_priv *tabla = snd_soc_codec_get_drvdata(codec);
 	struct snd_ctrl_data *snd_data = tabla->ctrl_data;
 
-	if (IS_ERR_OR_NULL(snd_data) || !snd_ctrl_data_handled(snd_data))
+	if (IS_ERR_OR_NULL(snd_data) || !snd_ctrl_data_handled(snd_data) ||
+	    !(snd_data->flags & SND_CTRL_BYPASS_IOCTL))
 		return 0;
 
 	/* Sort in use-frequency order */
@@ -8514,6 +8515,7 @@ static int tabla_codec_probe(struct snd_soc_codec *codec)
 	tabla->ctrl_data->name = codec->name;
 	tabla->ctrl_data->read = &tabla_read;
 	tabla->ctrl_data->write = &tabla_write;
+	tabla->ctrl_data->flags = SND_CTRL_BYPASS_IOCTL;
 
 	ret = snd_ctrl_register(tabla->ctrl_data);
 	if (IS_ERR_VALUE(ret)) {